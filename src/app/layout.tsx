8e2d64b5345444e4e35234f3a5c1b3b135639654
iadokonat <iadokonat0106@gmail.com>
2024-01-20 21:16:49 +0900

    add: Headerを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { Header } from '@/components/header'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -13,7 +14,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <body>
+        <Header />
+        <main className="pt-4">
+
+        {children}
+        </main>
+      </body>
     </html>
   )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,16 @@
+import Link from 'next/link'
+import { Container } from './container'
+
 export const Header = () => {
-  return <header>Auth Sample</header>
+  return (
+    <header className='border-b'>
+      <Container>
+        <div className="flex items-center justify-between">
+          <h1 className="py-4 text-lg font-bold">
+            <Link href="/">Auth Sample</Link>
+          </h1>
+        </div>
+      </Container>
+    </header>
+  )
 }
