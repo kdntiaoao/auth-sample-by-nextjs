1577e0b8725eac4d09a8ea96024aeba2accb3c5e
iadokonat <iadokonat0106@gmail.com>
2024-01-21 13:39:51 +0900

    feat: Googleでのログイン

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,14 +1,16 @@
 'use client'
 
+import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 import { Button } from '@/components/ui/button'
 import { Skeleton } from '@/components/ui/skeleton'
+import { Separator } from "@/components/ui/separator"
 import { AuthCard } from '@/components/auth-card'
 import { AuthCardSkeleton } from '@/components/auth-card-skeleton'
 import { useUser } from '@/hooks/use-user'
-import { createUserWithEmailAndPassword } from '@/lib/auth'
+import { createUserWithEmailAndPassword, signInWithGoogle } from '@/lib/auth'
 
 export default function Signin() {
   const router = useRouter()
@@ -40,9 +42,16 @@ export default function Signin() {
         {loading ? (
           <Skeleton className="h-8 w-40 max-w-full" />
         ) : (
+          <>
           <Button asChild size="sm" variant="link">
             <Link href="/signin">アカウントをお持ちの方はこちら</Link>
           </Button>
+          <Separator className='my-4' />
+          <Button className='flex items-center gap-2 mx-auto' onClick={signInWithGoogle}>
+            <Image src="/google.png" width={20} height={20} alt="Googleのロゴ" />
+            Sign in with Google
+          </Button>
+          </>
         )}
       </div>
     </div>
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,17 +1,20 @@
 import {
   type User,
   type Unsubscribe,
+  GoogleAuthProvider,
   createUserWithEmailAndPassword as createUser,
-  signInWithEmailAndPassword as signIn,
+  signInWithEmailAndPassword as signInWithEmailAndPasswordByFirebase,
+  signInWithPopup,
   onAuthStateChanged as onAuthStateChangedByFirebase,
   signOut as signOutByFirebase,
 } from 'firebase/auth'
 import { auth } from './firebase/client'
 
-export const createUserWithEmailAndPassword = async (
-  email: string,
-  password: string,
-): Promise<{ user: User; error: null } | { user: null; error: Error }> => {
+type UserResult = { user: User; error: null } | { user: null; error: Error }
+
+const provider = new GoogleAuthProvider()
+
+export const createUserWithEmailAndPassword = async (email: string, password: string): Promise<UserResult> => {
   try {
     const userCredential = await createUser(auth, email, password)
     const user = userCredential.user
@@ -24,15 +27,25 @@ export const createUserWithEmailAndPassword = async (
   }
 }
 
-export const signInWithEmailAndPassword = async (
-  email: string,
-  password: string,
-): Promise<{ user: User; error: null } | { user: null; error: Error }> => {
+export const signInWithEmailAndPassword = async (email: string, password: string): Promise<UserResult> => {
   try {
-    const userCredential = await signIn(auth, email, password)
+    const userCredential = await signInWithEmailAndPasswordByFirebase(auth, email, password)
     const user = userCredential.user
     return { user, error: null }
   } catch (error) {
+    if (error instanceof Error) {
+      return { user: null, error }
+    }
+    return { user: null, error: new Error('Unknown error') }
+  }
+}
+
+export const signInWithGoogle = async (): Promise<UserResult> => {
+  try {
+    const result = await signInWithPopup(auth, provider)
+    return { user: result.user, error: null }
+  } catch (error) {
+    console.error(error)
     if (error instanceof Error) {
       return { user: null, error }
     }
